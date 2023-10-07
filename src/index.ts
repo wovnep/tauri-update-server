03ea01b2709dec269f7a04e54ee59603426686a3
wovnep <122073572+wovnep@users.noreply.github.com>
2023-10-07 10:46:49 +0530

    fixing windows username override issue

--- src/index.ts
+++ src/index.ts
@@ -18,18 +18,18 @@ app.get('/update', async (req: Request<{}, {}, {}, Parameters>, res: Response) =
         const latest = await github();
         if (latest.message === 'Not Found') {
             res.status(204).send();
-        } else {
-            if (compare(latest.tag_name, req.query.current_version, '>')) {
-                const version = process.env.TAG_STRUCTURE ? latest.tag_name.split(process.env.TAG_STRUCTURE)[1] : latest.tag_name;
-                const release = getReleases(latest.assets, req.query.target, req.query.arch);
-                if (Object.keys(release).length !== 0) {
-                    res.status(200).send(await template(release, version, req.query.current_version));
-                } else {
-                    res.status(204).send();
-                }
+        }
+        if (compare(latest.tag_name, req.query.current_version, '>')) {
+            const version = process.env.TAG_STRUCTURE ? latest.tag_name.split(process.env.TAG_STRUCTURE)[1] : latest.tag_name;
+            const release = getReleases(latest.assets, req.query.target, req.query.arch);
+            if (Object.keys(release).length !== 0) {
+                res.status(200).send(await template(release, version, req.query.current_version));
             } else {
                 res.status(204).send();
             }
+        } else {
+            res.status(204).send();
+        
         }
     } else {
         res.status(400).send({
